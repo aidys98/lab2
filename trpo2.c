2aebe98fc557563a341f01f815c5622b9e487599
mahmud0505 <mbakhodurov@gmail.com>
2017-04-05 13:12:02 +0700

    lab2

---
+++ trpo2.c
@@ -0,0 +1,66 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	int b;
+	float x,s;
+	printf("Vvedite summu vklada u cpok: ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if (a>=10000 && a<=100000) {
+	  if (b>=0 && b<=30) 
+	  {
+	   	x=(a*10)/100;
+        s=a-x;
+        printf("\n %0.3f",s);
+	  }         
+	  if (b>=31 && b<=120)
+	  {
+	   	x=(a*2)/100;
+        s=a+x;
+        printf("\n %0.3f",s);
+	  }
+	  if (b>=121 && b<=240)
+	  {
+	   	x=(a*6)/100;
+        s=a+x;
+        printf("\n %0.3f",s);
+	  } 
+	  if (b>=241 && b<=365)
+	  {
+	   	x=(a*12)/100;
+        s=a+x;
+        printf("\n %0.3f",s);
+	  }       
+    }
+    if (a>=10000 && a>100000) {
+	  if (b>=0 && b<=30) 
+	  {
+	   	x=(a*10)/100;
+        s=a-x;
+        printf("\n %0.3f",s);
+	  }         
+	  if (b>=31 && b<=120)
+	  {
+	   	x=(a*3)/100;
+        s=a+x;
+        printf("\n %0.3f",s);
+	  }
+	  if (b>=121 && b<=240)
+	  {
+	   	x=(a*8)/100;
+        s=a+x;
+        printf("\n %0.3f",s);
+	  } 
+	  if (b>=241 && b<=365)
+	  {
+	   	x=(a*15)/100;
+        s=a+x;
+        printf("\n %0.3f",s);
+	  }       
+    }
+    system("pause");
+    return 0;
+}
