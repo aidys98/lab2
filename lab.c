87b7ecfcc85bc549d063f33599254dae0e97ed67
Суван Айдыс Викторович <ip614s19@mwp01.csc.local>
2017-04-12 14:55:58 +0700

    Vvod dannyh

--- lab.c
+++ lab.c
@@ -3,7 +3,12 @@
 #include<math.h>
 int main()
 {
-  
+  int a;
+  int b;
+  float x,s;
+  printf("Vvedite summu vklada u cpok: ");
+  scanf("%d",&a);
+  scanf("%d",&b);
   system("pause");
   return 0;
 }
