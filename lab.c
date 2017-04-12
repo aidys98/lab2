d938b2d4efc4712e7a4c12a4d8f7e2e94fbb7ebd
Суван Айдыс Викторович <ip614s19@mwp01.csc.local>
2017-04-12 15:10:31 +0700

    Gotovaya programma

--- lab.c
+++ lab.c
@@ -9,6 +9,32 @@ int main()
   printf("Vvedite summu vklada u cpok: ");
   scanf("%d",&a);
   scanf("%d",&b);
+  if (a>=10000 && a<=100000) {
+    if (b>=0 && b<=30)
+      {
+	x=(a*10)/100;
+	s=a-x;
+	printf("\n %0.3f",s);
+      }
+    if (b>=31 && b<=120)
+      {
+	x=(a*2)/100;
+	s=a+x;
+	printf("\n %0.3f",s);
+      }
+    if (b>=121 && b<=240)
+      {
+	x=(a*8)/100;
+	s=a+x;
+	printf("\n %0.3f",s);
+      }
+    if (b>=241 && b<=365)
+      {
+	x=(a*15)/100;
+	s=a+x;
+	printf("\n %0.3f",s);
+      }
+  }
   system("pause");
   return 0;
 }
